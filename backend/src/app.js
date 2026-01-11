c2f37023bf6c5d6e230afb56c7f4f5cfd9634e29
dakshgola <dakshgola0905@gmail.com>
2026-01-11 08:08:34 +0530

    Fix CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://foodflux-frontend.onrender.com"
+  ],
+  credentials: true
 }));
 
 app.use(cookieParser());
